b775c80b7e548c0815361ce1f01ad388006a1d90
nikomaniac <niko.sky.lan@gmail.com>
2018-10-03 09:51:37 +0800

    create shelf

--- react-shopping-cart/src/containers/App.js
+++ react-shopping-cart/src/containers/App.js
@@ -1,5 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import {Provider} from "react-redux";
+import { Provider } from 'react-redux';
 
+import store from '../store/index';
+
+
+import Shelf from '../components/shelf/Shelf';
+// import Footer from '../components/Footer';
+// import FloatCart from './../components/floatCart/FloatCart';
+
+// import Corner from '../components/github/Corner';
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <div className="App">
+                    {/* <Corner /> */}
+                    <main>
+                        <Shelf />
+                    </main>
+                    {/* <Footer /> */}
+                    {/* <FloatCart /> */}
+                </div>
+            </Provider>
+        );
+    }
+}
+
+export default App;
